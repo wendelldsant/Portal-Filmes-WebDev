6e57d1325b7bde4998a752984cb262c5309d80d4
Wendell dos Santos <wendell_dsantos@hotmail.com>
2024-09-24 21:02:30 -0300

    changes-aula2409

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
+import movies from "../data/movies.json"
+import MovieCard from "../components/MovieCard";
+import { list } from "postcss";
+
 export default function MovieListPage(){
+
+    // const [textBotao, setTextBotao ] = useState()
+
+    // const handleClick = () => {
+        
+    //     setContador((prev)=> prev+1)
+    //     console.log(contador)
+    // }
+
+    const [search, setSearch] = useState('')
+    const[listaFilmes, setListaFilmes] = useState(movies);
+
+    const handleSearch = (event) =>{
+        setSearch(event.target.value)
+        const filmesFiltrados = movies.filter(filme => ( filme.titulo).includes(search))
+        setListaFilmes(filmesFiltrados)
+        console.log(listaFilmes)
+        if (listaFilmes === ''){
+            setListaFilmes (movies)
+        }   
+    }
     return(
         <>
-        <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+            <h2>Veja a Lista Completa de Filmes</h2>   
+            <input 
+            className="text-black"
+            type="text" 
+            name="search" 
+            id="search" 
+            placeholder="Digite o filme aqui"
+            value={search}
+            onChange={handleSearch}
+            />
+            <div className="flex">
+                {
+                    listaFilmes
+                        .map(movie=> (
+                            <MovieCard key={movie.id} {...movie}/>
+                    ))
+                            
+                }
+            </div>
         </>
     )
-}
+}   
